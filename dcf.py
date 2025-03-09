20cde70faa89a738874049b9b7a93d6a7e9112cc
Jorden Anfinson <anfins_j1@denison.edu>
2025-03-08 21:21:52 -0500

    added some additional values to analyze

--- dcf.py
+++ dcf.py
@@ -8,14 +8,21 @@ def getStockInfo(ticker):
     stockInfo = stock.info
     #print(stockInfo.keys())
 
-    print(ticker)
+    print(f"Ticker: {ticker}")
     print(f"Current Price: ${stockInfo['currentPrice']:,.2f}")
-    print(stockInfo["financialCurrency"])
-    print(stockInfo["exchange"])
+    print(f"Previous Close: ${stockInfo['previousClose']:,.2f}")
+    print(f"Shares Outstanding: {stockInfo['sharesOutstanding']:,.0f}")
+    print(f"Shares Short: {stockInfo['sharesShort']:,.0f}")
+    print(f"Short Ratio: {stockInfo['shortRatio']:.2f}")
+    print(f"Currency: {stockInfo['financialCurrency']}")
+    print(f"Earnings Growth: ${stockInfo['earningsGrowth']*100:,.2f}%")
+    print(f"52 Week Range: {stockInfo['fiftyTwoWeekRange']}")
+    print(f"Held by Institutions: {stockInfo['heldPercentInstitutions'] * 100:.2f}%")
+    print(f"Held by Insiders: {stockInfo['heldPercentInsiders'] * 100:.2f}%")
     print(f"Market Cap: ${stockInfo['marketCap']:,.2f}")
-    print(stockInfo["forwardPE"])
+    print(f"Forward P/E: {stockInfo['forwardPE']}")
     print(f"Free Cash Flow: ${stockInfo['freeCashflow']:,.2f}")
-
+    print("-----------------------------")
 
     
 def main():
