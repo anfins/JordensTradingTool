137f1e7606f5ceef890b2bc240398da93f36e58c
Jorden Anfinson <anfins_j1@denison.edu>
2025-03-09 10:55:25 -0400

    Added ability to plot data, it will be saved to the plots folder

--- Analysis.py
+++ Analysis.py
@@ -6,18 +6,20 @@ import matplotlib.pyplot as plt
 
 def getStockInfo(ticker):
     stock = yf.Ticker(ticker)   
+    
     stockInfo = stock.info
-
     try:
         stockData = {
             "Market Cap": stockInfo["marketCap"],
             "Free Cash Flow": stockInfo["freeCashflow"],    
-            "Total Revenue": stockInfo["totalRevenue"],
+            "Net Income": stockInfo["netIncomeToCommon"],
+            "Total Debt": stockInfo["totalDebt"],
             "Current Price": stockInfo["currentPrice"],
             "52 Week Range": stockInfo["fiftyTwoWeekRange"],
             "52 Week High Change": stockInfo["fiftyTwoWeekHighChange"],
-            "Held by Institutions": stockInfo["heldPercentInstitutions"],
-            "Held by Insiders": stockInfo["heldPercentInsiders"],
+            "Return on Equity": stockInfo["returnOnEquity"],
+            "Held by Institutions": stockInfo["heldPercentInstitutions"] * 100,
+            "Held by Insiders": stockInfo["heldPercentInsiders"] * 100,
             "Forward P/E": stockInfo["forwardPE"],
         }
         return stockData
@@ -25,19 +27,21 @@ def getStockInfo(ticker):
         print(f"Error: {e}")
 
 
-def plotStockData(stockData, passedVal):
-    stockDf = pd.DataFrame(stockData)
-    stockDf[passedVal] = stockDf[passedVal].astype(float)
+def plotData(stockDf, passedVal):
+    stockDf = stockDf.dropna(subset=[passedVal])
     
-    plt.figure(figsize=(10, 6))
-    stockDf['Market Cap'].plot(kind='bar')
-    plt.title('Market Cap of Selected Stocks')
-    plt.xlabel('Stock Ticker')
-    plt.ylabel('Market Cap (in billions)')
+    # Plot Market Cap
+    plt.figure(figsize=(12, 6))
+    stockDf[passedVal].plot(kind="bar", color="skyblue", edgecolor="black")
+    plt.title(passedVal +  " of Selected Stocks")
+    plt.xlabel("Stock")
+    plt.ylabel(passedVal + " (in USD)")
     plt.xticks(rotation=45)
-    plt.show()
-
-
+    plt.grid(axis="y", linestyle="--", alpha=0.7)
+    plt.tight_layout()  # Added to ensure labels fit properly
+    # Save the plot instead of showing it
+    plt.savefig(f"Plots/{passedVal.replace('/', '_')}_plot.png")
+    plt.close()  # Close the figure to free memory
     
 def main():
     # Create a Stock object
@@ -46,7 +50,15 @@ def main():
     for stock in stocks:
         stockData[stock] = getStockInfo(stock)
 
-    plotStockData(stockData, "Market Cap")
+
+    stockDf = pd.DataFrame(stockData).T
+    plotData(stockDf, "Market Cap")
+    plotData(stockDf, "Free Cash Flow")
+    plotData(stockDf, "Net Income")
+    plotData(stockDf, "Total Debt")
+
+
+
 
 if __name__ == '__main__':
     main()
